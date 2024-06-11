570b50c763bf3ce64e0c3da63d126a634a76fac6
Mikodev350 <adev3944@gmail.com>
2024-06-12 00:43:56 +0100

    fixing bugs

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,10 +1,10 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import { useLocation } from "react-router-dom";
-import { fetchAdvancedSearch, fetchUserSearch, setFilterType } from "../../redux/features/search-slice";
+import { fetchAdvancedSearch, fetchUserSearch, setFilterType, clearResults } from "../../redux/features/search-slice";
 import useStorage from "../../hooks/useStorage";
 import SidebarDesktop from "../sideBar/SideBarDesktop/SidebarDesktop";
 import SideBarMobile from "../sideBar/sideBarMobile/SideBarMobile";
@@ -42,7 +42,7 @@ const Layout = ({ fullcontent, backgroundColorIdentification, children }) => {
     }
   };
 
-  React.useEffect(() => {
+  useEffect(() => {
     if (currentRoute === "STUDENT") {
       setType("type", "DASHEBOARD_STUDENT");
     } else if (currentRoute === "TEACHER") {
@@ -51,7 +51,10 @@ const Layout = ({ fullcontent, backgroundColorIdentification, children }) => {
       if (role === "STUDENT") setType("type", "SETTINGS_STUDENT");
       else setType("type", "SETTINGS_TEACHER");
     }
-  }, [currentRoute, role, setType]);
+
+    // Clear search results and filters when the location changes
+    dispatch(clearResults());
+  }, [currentRoute, role, setType, dispatch, location.pathname]);
 
   const backgroundColor = backgroundColorIdentification ? "white" : "#F1F1F1";
 
@@ -91,101 +94,3 @@ const Layout = ({ fullcontent, backgroundColorIdentification, children }) => {
 };
 
 export default Layout;
-
-
-
-// import React from "react";
-// import { useDispatch, useSelector } from "react-redux";
-// import Container from "react-bootstrap/Container";
-// import Row from "react-bootstrap/Row";
-// import Col from "react-bootstrap/Col";
-// import { useLocation } from "react-router-dom";
-// import { fetchAdvancedSearch, fetchUserSearch } from "../../redux/features/search-slice";
-
-// import useStorage from "../../hooks/useStorage";
-// import SidebarDesktop from "../sideBar/SideBarDesktop/SidebarDesktop";
-// import SideBarMobile from "../sideBar/sideBarMobile/SideBarMobile";
-// import SocialMediaNavbar from "../header/SocialMediaNavbar";
-// import ResourceResults from "../search-results/ResourceResults";
-// import UserResults from "../search-results/UserResults";
-
-// const Layout = ({ fullcontent, backgroundColorIdentification, children }) => {
-//   const role = React.useMemo(() => {
-//     return localStorage.getItem("role");
-//   }, []);
-//   const location = useLocation();
-//   const currentRoute = location.pathname.split("/")[1].toUpperCase();
-//   const [, , setType] = useStorage({ key: "type" });
-
-//   const dispatch = useDispatch();
-//   const searchResults = useSelector((state) => state.search.results);
-//   const searchStatus = useSelector((state) => state.search.status);
-
-//   const handleSearchResults = (filters) => {
-//     const { parcoursFilter, moduleFilter, lessonFilter, userType, ...rest } = filters;
-
-//     const params = {
-//       ...rest,
-//       parcours: parcoursFilter ? parcoursFilter.join(",") : '',
-//       modules: moduleFilter ? moduleFilter.join(",") : '',
-//       lessons: lessonFilter ? lessonFilter.join(",") : '',
-//     };
-
-//     if (userType) {
-//       dispatch(fetchUserSearch(params));
-//     } else {
-//       dispatch(fetchAdvancedSearch(params));
-//     }
-//   };
-
-//   if (currentRoute === "STUDENT") {
-//     setType("type", "DASHEBOARD_STUDENT");
-//   } else if (currentRoute === "TEACHER") {
-//     setType("type", "DASHEBOARD_TEACHER");
-//   } else if (currentRoute === "SETTINGS") {
-//     if (role === "STUDENT") setType("type", "SETTINGS_STUDENT");
-//     else {
-//       setType("type", "SETTINGS_TEACHER");
-//     }
-//   }
-
-//   const backgroundColor = backgroundColorIdentification ? "white" : "#F1F1F1";
-
-//   console.log("----------------------------------------")
-//     console.log(searchResults);
-//   console.log("----------------------------------------")
-
-//   return (
-//     <>
-//       <SocialMediaNavbar onFilterChange={handleSearchResults} />
-//       <aside>
-//         <SideBarMobile />
-//       </aside>
-//       <main style={{ backgroundColor, minHeight: "100vh" }}>
-//         <Container>
-//           {fullcontent ? (
-//             children
-//           ) : (
-//             <Row>
-//               <Col md={3} className="rc-side-bar">
-//                 <SidebarDesktop />
-//               </Col>
-//               <Col md={8}>
-//                 {children}
-//                 {searchStatus === "loading" && <div>Loading...</div>}
-//                 {searchResults.length > 0 && searchResults[0].type === 'student' && (
-//                   <UserResults results={searchResults} />
-//                 )}
-//                 {searchResults.length > 0 && searchResults[0].type !== 'student' && (
-//                   <ResourceResults results={searchResults} />
-//                 )}
-//               </Col>
-//             </Row>
-//           )}
-//         </Container>
-//       </main>
-//     </>
-//   );
-// };
-
-// export default Layout;
--- src/redux/features/search-slice.js
+++ src/redux/features/search-slice.js
@@ -48,6 +48,11 @@ const searchSlice = createSlice({
     setFilterType: (state, action) => {
       state.filterType = action.payload;
     },
+    clearResults: (state) => {
+      state.results = [];
+      state.status = "idle";
+      state.filterType = "resource"; // reset filter type
+    },
   },
   extraReducers: (builder) => {
     builder
@@ -76,81 +81,6 @@ const searchSlice = createSlice({
   },
 });
 
-export const { setFilterType } = searchSlice.actions;
+export const { setFilterType, clearResults } = searchSlice.actions;
 
 export default searchSlice.reducer;
-
-// import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-// import axios from "axios";
-// import { getToken } from "../../util/authUtils";
-
-// export const fetchAdvancedSearch = createAsyncThunk(
-//   "search/fetchAdvancedSearch",
-//   async (params) => {
-//     const token = getToken();
-//     const { data } = await axios.get(
-//       `http://localhost:1337/api/custom-search/advanced`,
-//       {
-//         params,
-//         headers: {
-//           Authorization: `Bearer ${token}`,
-//         },
-//       }
-//     );
-//     return data;
-//   }
-// );
-
-// export const fetchUserSearch = createAsyncThunk(
-//   "search/fetchUserSearch",
-//   async (params) => {
-//     const token = getToken();
-//     const { data } = await axios.get(
-//       `http://localhost:1337/api/custom-search/users`,
-//       {
-//         params,
-//         headers: {
-//           Authorization: `Bearer ${token}`,
-//         },
-//       }
-//     );
-//     return data;
-//   }
-// );
-
-// const searchSlice = createSlice({
-//   name: "search",
-//   initialState: {
-//     results: [],
-//     status: "idle",
-//     error: null,
-//   },
-//   reducers: {},
-//   extraReducers: (builder) => {
-//     builder
-//       .addCase(fetchAdvancedSearch.pending, (state) => {
-//         state.status = "loading";
-//       })
-//       .addCase(fetchAdvancedSearch.fulfilled, (state, action) => {
-//         state.status = "succeeded";
-//         state.results = action.payload;
-//       })
-//       .addCase(fetchAdvancedSearch.rejected, (state, action) => {
-//         state.status = "failed";
-//         state.error = action.error.message;
-//       })
-//       .addCase(fetchUserSearch.pending, (state) => {
-//         state.status = "loading";
-//       })
-//       .addCase(fetchUserSearch.fulfilled, (state, action) => {
-//         state.status = "succeeded";
-//         state.results = action.payload;
-//       })
-//       .addCase(fetchUserSearch.rejected, (state, action) => {
-//         state.status = "failed";
-//         state.error = action.error.message;
-//       });
-//   },
-// });
-
-// export default searchSlice.reducer;
