cd6fd610c33c23345181864352a77ea04392235b
hocineismail <hocineismail75@gmail.com>
2024-08-28 18:07:50 +0100

    Added buttonto scroll to up

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -14,7 +14,49 @@ import Footer from "../../components/footer/Footer";
 import SideBarMobile from "../../components/sideBar/sideBarMobile/SideBarMobile";
 import CustomHeader from "./other-header";
 
+import styled from "styled-components";
+import { FaArrowUp } from "react-icons/fa";
+
+const ButtonTop = styled.div`
+  width: 45px;
+  height: 45px;
+  cursor: pointer;
+  display: flex;
+  align-items: center; /* Center vertically */
+  justify-content: center; /* Center horizontally */
+  position: fixed;
+  right: 10px;
+  bottom: 10px;
+  z-index: 100;
+  background-color: #10266f;
+  border-radius: 12px;
+  &:hover {
+    opacity: 0.8;
+  }
+`;
 export default function Home() {
+  const [isVisible, setIsVisible] = React.useState(false);
+
+  React.useEffect(() => {
+    const toggleVisibility = () => {
+      if (window.pageYOffset > 300) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    };
+
+    window.addEventListener("scroll", toggleVisibility);
+
+    return () => window.removeEventListener("scroll", toggleVisibility);
+  }, []);
+
+  const handleScrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
   return (
     <>
       <Helmet>
@@ -30,8 +72,8 @@ export default function Home() {
       <section id="why">
         <WhySection />
       </section>
-       {/* ************ app + ens section ******* */}
-       <section id="testimonial">
+      {/* ************ app + ens section ******* */}
+      <section id="testimonial">
         <TestimonialSection />
       </section>
       {/* ************ fonctionalite section ******* */}
@@ -54,6 +96,11 @@ export default function Home() {
       <br />
       <br />
       <Footer />
+      {isVisible && (
+        <ButtonTop onClick={handleScrollToTop}>
+          <FaArrowUp color="white" size={29} />
+        </ButtonTop>
+      )}
     </>
   );
 }
